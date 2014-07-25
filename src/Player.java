ebd1f4c155f50f29a2bae125d87c2bf6275812ad
Joanne Lee <leekkww@hotmail.com>
2014-07-24 21:18:58 -0700

    added useful things

--- src/Player.java
+++ src/Player.java
@@ -7,9 +7,11 @@ public class Player {
 
     private Hand hand;
     private int points = 0;
+    private int level = 2;
     private ArrayList<Player> friends;
     private boolean napolean;
 
     public void addPoints(int points) {this.points+=points;}
-
+    public int getLevel(){return level;}
+    public int getPoints() {return points;}
 }
