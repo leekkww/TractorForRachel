ff135b5f553855ed8107d7c3616e456ab174f87f
Joanne Lee <leekkww@hotmail.com>
2014-07-24 20:52:34 -0700

    Added basic variables and states for player

--- src/Player.java
+++ src/Player.java
@@ -5,7 +5,10 @@ import java.util.*;
 public class Player {
 
     private Hand hand;
-    private int points;
+    private int points = 0;
     private ArrayList<Player> friends;
+    private boolean napolean;
+
+    public void addPoints(int points) {this.points+=points};
 
 }
