93f0b35a4cdee476d7548a236a635d104d4f30fd
Joanne Lee <leekkww@hotmail.com>
2014-07-25 15:52:33 -0700

    improve on take method

--- src/GameState.java
+++ src/GameState.java
@@ -5,8 +5,32 @@ import java.util.*;
 
 public class GameState {
 
-    private ArrayList<Card> played; //does this store all cards that have been played?
+    private ArrayList<Trick> played; //stores all the tricks that have been played
     private Player taking; //who is taking the points?
     private Card.Suit playedSuit; //what suit was played first
+
+    public void take() {
+        //this should happen after all the players have gone
+        //compare to see who is boss here
+
+
+
+        //default for now
+        taking = played.get(0).getPlayer();
+
+        int points = 0;
+        for(Trick t : played)
+        {
+            points += t.pointValue();
+        }
+        taking.addPoints(points);
+    }
+
+    //reset after each round
+    public void reset() {
+        played.clear();
+        taking = null;
+        playedSuit = null;
+    }
 
 }
