5b78459edc2498317a3a5c4465199c3231b1a66a
seanshi31 <seanshi31@gmail.com>
2014-07-24 20:42:31 -0700

    Update TractorGame.java
    dumby code halp
    added relevant/irrelevant stuff storing the state of the game

--- src/TractorGame.java
+++ src/TractorGame.java
@@ -1,7 +1,15 @@
 /**
  * Created by Jolee on 7/18/2014.
  */
+ 
+ import java.util.*;
+ 
 public class TractorGame {
-
-//su
+	
+	private GameState gameState;
+	private ArrayList<Player> players;
+	private ArrayList<Card> deck;
+	private Player firstPlayer;
+	private Suit trump;
+	
 }
