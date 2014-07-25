d7c41deb09f5c1f3d9538cbe930ca538d1d6055c
Joanne Lee <leekkww@hotmail.com>
2014-07-24 20:41:46 -0700

    Card, Hand, Player added

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Jolee on 7/24/2014.
+ */
+public class Card {
+
+    public enum Value {
+        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+    }
+
+    public enum Suit {
+        CLUBS, DIAMONDS, HEARTS, SPADES
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,11 @@
+import java.util.*;
+/**
+ * Created by Jolee on 7/24/2014.
+ */
+public class Player {
+
+    private Hand hand;
+    private int points;
+    private ArrayList<Player> friends;
+
+}
