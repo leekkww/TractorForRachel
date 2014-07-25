8fb974c915be28a763d18e8447789917b72c0156
Joanne Lee <leekkww@hotmail.com>
2014-07-24 21:44:20 -0700

    added some useful things someone who knows the game well should do this

--- src/Hand.java
+++ src/Hand.java
@@ -1,16 +1,28 @@
-import java.util.*;
 /**
  * Created by Jolee on 7/24/2014.
  */
+import java.util.*;
+
 public class Hand {
 
     private ArrayList<Card> hand;
 
-    public ArrayList<Card> validPlays(ArrayList<Card> initPlay)
+    /**
+     * checks if a play is valid at the start of a round
+     */
+    public boolean isValidPlay(ArrayList<Card> play)
+    {
+        if(play.size()==1) {return true;}
+        //if played hand is bigger than 1, check that all are of the same suit
+        else return false;
+    }
+
+    public boolean isValidPlay(ArrayList<Card> play, ArrayList<Card> initPlay, Card.Suit currentSuit)
     {
         for(int i = 0; i < initPlay.size(); ++i)
         {
-
+            //initPlay.get(1).isTrump();
         }
+        return false;
     }
 }
