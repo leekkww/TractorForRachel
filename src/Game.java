910647930772c1523f3cd75bf145ba4a4e730e79
Joanne Lee <leekkww@hotmail.com>
2014-08-07 16:52:01 -0700

    hmm final things are good for your health

--- src/Game.java
+++ src/Game.java
@@ -5,6 +5,8 @@ import java.util.*;
 
 public class Game {
 
+    private final int NUM_PLAYERS = 4;
+
     private ArrayList<Player> players;
 
     public void initialize() {
@@ -12,11 +14,11 @@ public class Game {
         //ask for names and stuff, but for now everyone is optimized
         //or maybe load saved game with previously established settings
         players = new ArrayList<Player>();
-        for(int i = 0; i<5; ++i) {
+        for(int i = 0; i < NUM_PLAYERS; ++i) {
             players.add(new Player());
         }
-        for(int i = 0; i<5; ++i) {
-            players.get(i).setNextPlayer(players.get((i+1)%5));
+        for(int i = 0; i < NUM_PLAYERS; ++i) {
+            players.get(i).setNextPlayer(players.get((i+1)% NUM_PLAYERS));
         }
         Round r = new Round(players);
     }
