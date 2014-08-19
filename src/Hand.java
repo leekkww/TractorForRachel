c3ccf3e710ddedc7d8af85a44ada3854fcc2718e
Joanne Lee <leekkww@hotmail.com>
2014-08-19 11:21:21 -0500

    stuff

--- src/Hand.java
+++ src/Hand.java
@@ -32,6 +32,10 @@ public class Hand {
         return false;
     }
 
+    public void sort() {
+        Collections.sort(hand);
+    }
+
     public Card get(int i) {
         return hand.get(i);
     }
